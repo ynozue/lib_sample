3061ef68e52b5cefa7c721b46196b8c1656cbef8
yasu <yasu@MacBook-Pro.local>
2018-05-14 14:39:31 +0900

    add main

--- main.go
+++ main.go
@@ -7,8 +7,18 @@ import (
 	"github.com/ynozue/lib_sample/subpackage_B"
 )
 
+type Main struct {
+	Name string
+}
+
+func (m Main) PrintMain() {
+	fmt.Println("Main [" + m.Name + "]")
+}
+
 func main() {
 	fmt.Println("Hello! [https://github.com/ynozue/lib_sample]")
+	m := Main{Name: "メイン"}
+	m.PrintMain()
 	a := subpackage_A.SubA{Name: "サブA"}
 	a.PrintA()
 	b := subpackage_B.SubB{Name: "サブB"}
